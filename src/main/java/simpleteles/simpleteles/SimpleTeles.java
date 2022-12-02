1df1ff93f140333be773f2e84796555e5a0f1dc4
miguel-l-b <miguelcoruj@gmail.com>
2022-12-02 16:45:28 -0300

    first commit

---
+++ src/main/java/simpleteles/simpleteles/SimpleTeles.java
@@ -0,0 +1,14 @@
+package simpleteles.simpleteles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleTeles extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
